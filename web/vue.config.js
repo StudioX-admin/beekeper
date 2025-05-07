03fa032865368e59d16897f3d9bf989d59cf9dc7
StudioX-admin <contact@studiox.co.kr>
2025-05-07 17:43:34 +0900

    Update vue.config.js

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,22 +1,5 @@
+
 module.exports = {
-  devServer: {
-    proxy: {
-      '/api': {
-        target: process.env.VUE_APP_API_URL || 'http://localhost:5000',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '/api'
-        }
-      }
-    }
-  },
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: `
-          @import "@/assets/scss/_variables.scss";
-        `
-      }
-    }
-  }
+  lintOnSave: false,
+  transpileDependencies: true
 }
