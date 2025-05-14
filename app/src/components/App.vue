eaca3ed0c16a6238995f9f90d3a8c38bb12b67a1
StudioX-admin <contact@studiox.co.kr>
2025-05-14 09:18:39 +0900

    Create App.vue

---
+++ app/src/components/App.vue
@@ -0,0 +1,29 @@
+<!-- app/src/components/App.vue -->
+<template>
+  <div id="app">
+    <div class="app-container">
+      <router-view />
+      <BottomNavigation v-if="isAuthenticated" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BottomNavigation from './BottomNavigation.vue'  // 수정된 컴포넌트 이름
+
+export default {
+  name: 'App',
+  components: {
+    BottomNavigation
+  },
+  computed: {
+    isAuthenticated() {
+      return !!localStorage.getItem('token')
+    }
+  }
+}
+</script>
+
+<style>
+/* 스타일 코드는 동일하게 유지 */
+</style>
