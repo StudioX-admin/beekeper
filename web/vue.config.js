0deeb513a07402d462a1fe164dd791d63323a7cc
StudioX-admin <contact@studiox.co.kr>
2025-05-14 14:29:20 +0900

    Update vue.config.js

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,8 +1,10 @@
+ // web/vue.config.js
 module.exports = {
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'assets',
   productionSourceMap: false,
+  lintOnSave: false,
   devServer: {
     proxy: {
       '/api': {
@@ -14,6 +16,8 @@ module.exports = {
   
   // chainWebpack 설정을 module.exports 객체 내부로 이동
   chainWebpack: config => {
+    config.module.rules.delete('eslint');
+    
     // SVG 로더 설정
     const svgRule = config.module.rule('svg');
     svgRule.uses.clear();
@@ -55,5 +59,18 @@ module.exports = {
           quality: 75
         }
       });
+
+      // 경로 별칭 추가
+    config.resolve.alias.set('@', require('path').join(__dirname, 'src'));
+  },
+  
+  // CSS 관련 설정 추가
+  css: {
+    loaderOptions: {
+      css: {
+        // CSS 모듈에서 변수 파일을 찾을 수 없는 오류 해결
+        importLoaders: 1
+      }
+    }
   }
 };
