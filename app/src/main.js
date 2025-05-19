1fa34bd9fb9bf95017bc130fb39f8bd9ab9d7c7a
StudioX-admin <contact@studiox.co.kr>
2025-05-19 16:15:33 +0900

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
 
-import './assets/main.css'
+import '@/assets/main.css'
 
 const app = createApp(App)
 
