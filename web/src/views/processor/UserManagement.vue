7ac80c4af13b139088df3c32777d7554c0bb4e73
StudioX-admin <contact@studiox.co.kr>
2025-05-22 12:55:27 +0900

    Add files via upload

---
+++ web/src/views/processor/UserManagement.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="user-management">
+    <h1>사용자 관리</h1>
+    <!-- 사용자 관리 기능 구현 예정 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserManagement'
+}
+</script>
+
+<style scoped>
+.user-management {
+  padding: 2rem;
+}
+</style> 
