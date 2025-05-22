377994d9863249298265e0e3c02f07e8e8ff2f77
StudioX-admin <contact@studiox.co.kr>
2025-05-22 09:07:08 +0900

    Update index.js

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { useAuthStore } from '@/store/auth'
+import { useAuthStore } from '@/store/modules/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
