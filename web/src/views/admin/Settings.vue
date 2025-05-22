f8b6810c3f0ca98426b4c011020293ff8cc28cb3
StudioX-admin <contact@studiox.co.kr>
2025-05-22 12:56:16 +0900

    Add files via upload

---
+++ web/src/views/admin/Settings.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="settings">
+    <h1>설정</h1>
+    <!-- 설정 기능 구현 예정 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Settings'
+}
+</script>
+
+<style scoped>
+.settings {
+  padding: 2rem;
+}
+</style> 
